8987c721207a44a7e743a103121bb7a64bf6db15
Pankaj Thakur <pankajvk246@gmail.com>
2023-06-19 02:19:14 +0530

    added  header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'crypto-checker';
+  selectedValue="INR";
+  sendCurrency(event: string){
+    console.log(event)
+  }
 }
